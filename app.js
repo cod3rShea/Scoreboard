0d1518cd701a756e6488e34bc4bf13444601549e
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2021-01-31 10:44:30 -0500

    test adding static text

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-
+const Header = () => {
+    return (
+        <header>
+            <h1>Scooreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
 
 ReactDOM.render(
-    header,
+    <Header />,
     document.getElementById('root')
   );
