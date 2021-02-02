dc220993420148764edf938fa4c9a14488e1759a
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2021-02-01 20:39:25 -0500

    update state to use previous state

--- app.js
+++ app.js
@@ -45,27 +45,27 @@ const Player = (props) => {
 class Counter extends React.Component {
 
     state = {
-        score: 20,
+        score: 0,
     };
     
-    decrementScore = () => {
-        this.setState({
-            score: this.state.score - 1
-        });
-    }
-
-    incrementScoore = () => {
-        this.setState({
-            score: this.state.score + 1
-        });
-    }
+    incrementScore = () => {
+        this.setState( prevState => ({
+          score: prevState.score + 1
+        }));
+      }
+    
+      decrementScore = () => {
+        this.setState( prevState => ({
+          score: prevState.score - 1
+        }));
+      }
 
     render() {
       return (
         <div className="counter">
           <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
           <span className="counter-score">{ this.state.score }</span>
-          <button className="counter-action increment" onClick={this.incrementScoore}> + </button>
+          <button className="counter-action increment" onClick={this.incrementScore}> + </button>
         </div>
       );
     }
