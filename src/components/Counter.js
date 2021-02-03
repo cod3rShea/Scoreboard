4d3e6efd00c521b95c2178a9eff0d92ff3995991
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2021-02-02 20:37:16 -0500

    break classes into their own components

---
+++ src/components/Counter.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class Counter extends React.Component {
+
+    state = {
+        score: 0,
+    };
+    
+    incrementScore = () => {
+        this.setState( prevState => {
+            if( isNaN(prevState.score) ){
+                return ({
+                    score: 1
+                });
+            } else {
+                return ({
+                    score: prevState.score + 1
+                });
+            }
+        });
+    }
+    
+    decrementScore = () => {
+        this.setState( prevState => {
+            if ( prevState.score === 0 || isNaN(prevState.score) ){
+                return ({
+                    score: "you lose"
+                });
+            } else {
+                return ({
+                    score: prevState.score - 1
+                });
+            }
+        });
+    }
+  
+    render() {
+      return (
+        <div className="counter">
+          <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+          <span className="counter-score">{ this.state.score }</span>
+          <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+        </div>
+      );
+    }
+  }
+
+export default Counter;
