51499b62f83564e6f3f92fb1efb15d8e0d27928e
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2021-02-04 21:19:04 -0500

    fix tabs spacing

--- src/components/App.js
+++ src/components/App.js
@@ -4,91 +4,97 @@ import Header from './Header';
 import Player from './Player';
 
 class App extends Component {
-  state = {
-	  players: [
-		  {
+	state = {
+		players: [
+			{
 			playerName: "Mike",
 			score: 20,
 			id: 1
-		   },
-		   {
+			},
+			{
 			playerName: "Shelbey",
 			score: 20,
 			id: 2
-		   },
-		   {
+			},
+			{
 			playerName: "Same",
 			score: 20,
 			id: 3
-		   },
-		   {
+			},
+			{
 			playerName: "Joey",
 			score: 20,
 			id: 4
-		   }
-	  ]
-  };
+			}
+		]
+	};
 
-  handleScoreChange = (index, delta) => {
-	this.setState( prevState => {
-		
-		const playerScore = prevState.players[index].score += delta;
+	handleScoreChange = (index, delta) => {
+		this.setState( prevState => {
+			
+			const playerScore = prevState.players[index].score += delta;
+			
+			if ( prevState.players[index].score === 0 ) {
+				return ({
+					score: prevState.players[index].score = "You Lose"
+				});
+			} else if ( isNaN(prevState.players[index].score) && delta === -1) {
+				return ({
+					score: prevState.players[index].score = "You Lose"
+				});
+			} else if ( isNaN(prevState.players[index].score) && delta === +1 ) {
+				return ({
+					score: prevState.players[index].score = 1
+				});
+			} else  {
+				return ({
+					score: playerScore
+				});
+			} 
 
-		if ( prevState.players[index].score === 0 ) {
-			return ({
-				score: prevState.players[index].score = "You Lose"
-			});
-		} else if ( isNaN(prevState.players[index].score) && delta === -1) {
-			return ({
-				score: prevState.players[index].score = "You Lose"
-			});
-		} else if ( isNaN(prevState.players[index].score) && delta === +1 ) {
-			return ({
-				score: prevState.players[index].score = 1
-			});
-		} else  {
-			return ({
-				score: playerScore
-			});
-		} 
-		
-	});
-}
+			return (
+				{
+					score: prevState.players[index].score += delta
+				}
+			)
 
-  handleRemovePlater = (id) => {
-	  this.setState( prevState => {
-		  return({
-			  players: prevState.players.filter( p => p.id !== id )
-		  })
-	  });
-  }
+		});
+	}
 
-  render() {
-	return (
-		<div className="scoreboard">
-			<Header 
-				title="Scoreboard" 
-				totalPlayers={this.state.players.length}     
-			/>
-			{/* Players List */}
+	handleRemovePlater = (id) => {
+		this.setState( prevState => {
+			return({
+				players: prevState.players.filter( p => p.id !== id )
+			})
+		});
+	}
 
-			{ this.state.players.map( (player, index) => {
-				return (
-					<Player
-						
-						playerName={player.playerName} 
-						score={player.score}
-						id={player.id}
-						key={player.id.toString()}
-						index={index}
-						changeScore={this.handleScoreChange}
-						removePlayer={this.handleRemovePlater}
-					/>
-				)
-			})}
-		</div>
-	);
-  }
+	render() {
+		return (
+			<div className="scoreboard">
+				<Header 
+					title="Scoreboard" 
+					totalPlayers={this.state.players.length}     
+				/>
+				{/* Players List */}
+
+				{ this.state.players.map( (player, index) => {
+					return (
+						<Player
+							
+							playerName={player.playerName} 
+							score={player.score}
+							id={player.id}
+							key={player.id.toString()}
+							index={index}
+							changeScore={this.handleScoreChange}
+							removePlayer={this.handleRemovePlater}
+						/>
+					)
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
